09d1f346f09645adbbce2440df89776e08be82ef
duongntrunsystem <duongnt1@runsystem.net>
2018-05-04 15:48:05 +0700

    added git reset

--- gmo_utils.sh
+++ gmo_utils.sh
@@ -1,31 +1,35 @@
-gmo_dev_pull() {
+gg_dev_pull() {
     git fetch && git checkout develop && git pull
 }
 
-gmo_cur_branch() {
+gg_cur_branch() {
     git branch -a | grep "*" | sed 's/* //g'
 }
 
-gmo_push() {
-    git push origin $(gmo_cur_branch)
+gg_push() {
+    git push origin $(gg_cur_branch)
 }
 
-gmo_add_commit() {
+gg_add_commit() {
     git add . && git commit -m "$1"
 }
 
-gmo_new_branch() {
+gg_new_branch() {
     git checkout -b "$1"
 }
 
-gmo_one_day_ago() {
+gg_one_day_ago() {
     date -v-1d +%Y-%m-%d
 }
 
-gmo_two_day_ago() {
+gg_two_day_ago() {
     date -v-2d +%Y-%m-%d
 }
 
-gmo_log() {
-    git log --author="duongnt1@runsystem.net" --after=$(gmo_two_day_ago) --before=$(gmo_one_day_ago)
+gg_log() {
+    git log --author="duongnt1@runsystem.net" --after=$(gg_two_day_ago) --before=$(gg_one_day_ago)
+}
+
+gg_reset() {
+    git reset --hard HEAD
 }
