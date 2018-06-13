70775d325676b2d7e90ffa534fb44ea432b878d5
duongntrunsystem <duongnt1@runsystem.net>
2018-06-13 16:59:02 +0700

    fixed print

--- gmo_utils.sh
+++ gmo_utils.sh
@@ -6,9 +6,7 @@ gg_welcome() {
 }
 
 gg_dev_pull() {
-  gg_print "Pull develop về đi anh em!"
   gg_fetch && gg_checkout develop && gg_pull
-  gg_print "Rồi! Giờ muốn làm gì thì làm đi!"
 }
 
 gg_branches() {
@@ -20,35 +18,35 @@ gg_cur_branch() {
 }
 
 gg_find_branch() {
-    gg_branches | grep "$1"
+  gg_branches | grep "$1"
 }
 
 gg_push() {
-    git push origin "$(gg_cur_branch)"
+  git push origin $(gg_cur_branch)
 }
 
 gg_merge() {
-    git merge "$1"
+  git merge "$1"
 }
 
 gg_merge_dev() {
-    gg_merge develop
+  gg_merge develop
 }
 
 gg_checkout() {
-    git checkout "$1"
+  git checkout "$1"
 }
 
 gg_add_commit() {
-    git add . && git commit -m "$1"
+  git add . && git commit -m "$1"
 }
 
 gg_new_branch() {
-    git checkout -b "$1"
+  git checkout -b "$1"
 }
 
 gg_diff() {
-    git diff
+  git diff
 }
 
 gg_diff_of() {
@@ -60,46 +58,35 @@ gg_diff_dev_cur() {
 }
 
 gg_log_of() {
-  gg_print "Check git của $1 nào!"
   git log --author="$1"
 }
 
 gg_log() {
-  gg_print "$(whoami) đã làm những gì nào?"
   git log --author=$(whoami)
 }
 
 gg_log_today() {
-  gg_print "Hôm nay $(whoami) đã làm được những gì nào?"
   git log --author=$(whoami) --after=$(gg_one_day_ago)
 }
 
 gg_log_yesterday() {
-  gg_print "Hôm qua $(whoami) đã làm được những gì nào?"
   git log --author=$(whoami) --after=$(gg_two_day_ago) --before=$(gg_one_day_ago)
 }
 
 gg_reset() {
-  gg_print "Đừng có xóa mạnh mẽ thế chứ?"
   git reset --hard HEAD
-  gg_print "Ôi méo kịp!"
 }
 
 gg_status() {
-  gg_print "Bây giờ git đang làm cái méo gì thế?"
   git status
 }
 
 gg_pull() {
-  gg_print "Hò zô ta kéo git về!"
   git pull
-  gg_print "Kéo git mệt vl ra!"
 }
 
 gg_fetch() {
-  gg_print "Kiểm tra xem git có cái méo gì mới không?"
   git fetch
-  gg_print "Cũng có cái gì đó gọi là mơi mới đó!"
 }
 
 gg_one_day_ago() {
@@ -111,17 +98,14 @@ gg_two_day_ago() {
 }
 
 gg_kmk_ad() {
-  gg_print "Lại vào KMK Beauty Passport Android nào!"
   cd ~/Desktop/projects/kmk_ad
 }
 
 gg_kmk_ios() {
-  gg_print "Lại vào KMK Beauty Passport iOS nào!"
   cd ~/Desktop/projects/kmk_ios
 }
 
 gg_kmk_doc() {
-  gg_print "Lại vào KMK Beauty Passport Documents nào!"
   cd ~/Desktop/projects/kmk_documents
 }
 
@@ -136,7 +120,7 @@ gg_video_to_gif() {
   ffmpeg -y -i $video_file -vf fps=$out_fps,scale=$out_scale:-1:flags=lanczos,palettegen $image_file
 
   ffmpeg -i $video_file -i $image_file -filter_complex "fps=$out_fps,scale=$out_scale:-1:flags=lanczos[x];[x][1:v]paletteuse" $gif_file
-    rm $image_file
+  rm $image_file
 
   gg_print "Chuyển khoản vào số tài khoản Vietcombank của ${whoami} nhé!"
 }
