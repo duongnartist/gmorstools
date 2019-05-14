6382945cea033e58ac867129cf44381f9e26df98
Duong Nguyen <duongnartist@Duongs-MacBook-Pro.local>
2019-05-14 11:52:48 +0700

    added brew installs

---
+++ adt_install_macos.sh
@@ -0,0 +1,20 @@
+
+echo "Install Applications"
+brew cask install google-chrome
+echo "Installed Google Chrome"
+brew cask install sublime-text
+echo "Installed Sublime Text"
+brew cask install visual-studio-code
+echo "Installed Visual Studio Code"
+brew cask install slack
+echo "Installed Slack"
+brew cask install diffmerge
+echo "Installed Diff Merge"
+brew cask install postman
+echo "Installed Postman"
+brew install git
+echo "Installed Git"
+brew install curl
+echo "Installed Curl"
+brew install wget
+echo "Installed Wget"
